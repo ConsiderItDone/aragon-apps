d44dfe46941bed86b0d4dff5796be7aa5acef961
ramil <ramilexe@gmail.com>
2021-12-09 21:32:26 +0300

    add avalanche network support

--- apps/voting/hardhat.config.js
+++ apps/voting/hardhat.config.js
@@ -1,1 +1,10 @@
-module.exports = require('@aragon/hardhat-config')
+const hardhatConfig = require('@aragon/hardhat-config')
+
+hardhatConfig.networks = {
+    ...hardhatConfig.networks,
+    dev: {
+        url: 'http://localhost:8545',
+    },
+};
+
+module.exports = hardhatConfig
